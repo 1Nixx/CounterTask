42ab5bceeb942c4cad96afe76f01f9cceda2b93b
Nikita Hripach <nikita.hripach@gmail.com>
2022-10-15 14:43:27 +0300

    Add project files.

---
+++ CounterTask/Controllers/CounterController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CounterTask.Controllers
+{
+	[Route("api/[controller]")]
+	[ApiController]
+	public class CounterController : ControllerBase
+	{
+	}
+}
